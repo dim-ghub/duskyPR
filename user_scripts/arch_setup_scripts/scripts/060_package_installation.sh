3a08e1351f4e92c6059411c7b23f7e092c3a9de9
dim-ghub <mlrudasill@gmail.com>
2026-02-23 00:29:25 -0600

    Replace Thunar with Nemo in package dependencies

--- user_scripts/arch_setup_scripts/scripts/060_package_installation.sh
+++ user_scripts/arch_setup_scripts/scripts/060_package_installation.sh
@@ -8,67 +8,67 @@
 
 # Group 1: Graphics & Drivers
 pkgs_graphics=(
-  "intel-media-driver" "vpl-gpu-rt" "mesa" "vulkan-intel" "mesa-utils" "intel-gpu-tools" "libva" "libva-utils" "vulkan-icd-loader" "vulkan-tools" "sof-firmware" "linux-firmware" "acpi_call-dkms"
+	"intel-media-driver" "vpl-gpu-rt" "mesa" "vulkan-intel" "mesa-utils" "intel-gpu-tools" "libva" "libva-utils" "vulkan-icd-loader" "vulkan-tools" "sof-firmware" "linux-firmware" "acpi_call-dkms"
 )
 
 # Group 2: Hyprland Core
 pkgs_hyprland=(
-"hyprland" "uwsm" "xorg-xwayland" "xdg-desktop-portal-hyprland" "xdg-desktop-portal-gtk" "xorg-xhost" "polkit" "hyprpolkitagent" "xdg-utils" "socat" "inotify-tools" "file"
+	"hyprland" "uwsm" "xorg-xwayland" "xdg-desktop-portal-hyprland" "xdg-desktop-portal-gtk" "xorg-xhost" "polkit" "hyprpolkitagent" "xdg-utils" "socat" "inotify-tools" "file"
 )
 
 # Group 3: GUI, Toolkits & Fonts
 pkgs_appearance=(
-"qt5-wayland" "qt6-wayland" "gtk3" "gtk4" "nwg-look" "qt5ct" "qt6ct" "qt6-svg" "qt6-multimedia-ffmpeg" "adw-gtk-theme" "matugen" "ttf-font-awesome" "ttf-jetbrains-mono-nerd" "noto-fonts-emoji" "sassc"
+	"qt5-wayland" "qt6-wayland" "gtk3" "gtk4" "nwg-look" "qt5ct" "qt6ct" "qt6-svg" "qt6-multimedia-ffmpeg" "adw-gtk-theme" "matugen" "ttf-font-awesome" "ttf-jetbrains-mono-nerd" "noto-fonts-emoji" "sassc"
 )
 
 # Group 4: Desktop Experience
 pkgs_desktop=(
-"waybar" "swww" "hyprlock" "hypridle" "hyprsunset" "hyprpicker" "swaync" "swayosd" "rofi" "libdbusmenu-qt5" "libdbusmenu-glib" "brightnessctl"
+	"waybar" "swww" "hyprlock" "hypridle" "hyprsunset" "hyprpicker" "swaync" "swayosd" "rofi" "libdbusmenu-qt5" "libdbusmenu-glib" "brightnessctl"
 )
 
 # Group 5: Audio & Bluetooth
 pkgs_audio=(
-"pipewire" "wireplumber" "pipewire-pulse" "playerctl" "bluez" "bluez-utils" "blueman" "bluetui" "pavucontrol" "gst-plugin-pipewire" "libcanberra"
+	"pipewire" "wireplumber" "pipewire-pulse" "playerctl" "bluez" "bluez-utils" "blueman" "bluetui" "pavucontrol" "gst-plugin-pipewire" "libcanberra"
 )
 
 # Group 6: Filesystem & Archives
 pkgs_filesystem=(
-"btrfs-progs" "compsize" "zram-generator" "udisks2" "udiskie" "dosfstools" "ntfs-3g" "gvfs" "gvfs-mtp" "gvfs-nfs" "gvfs-smb" "xdg-user-dirs" "usbutils" "gnome-disk-utility" "unzip" "zip" "unrar" "7zip" "cpio" "file-roller" "rsync" "thunar" "thunar-archive-plugin" "thunar-volman" "tumbler" "ffmpegthumbnailer" "webp-pixbuf-loader" "poppler-glib"
+	"btrfs-progs" "compsize" "zram-generator" "udisks2" "udiskie" "dosfstools" "ntfs-3g" "gvfs" "gvfs-mtp" "gvfs-nfs" "gvfs-smb" "xdg-user-dirs" "usbutils" "gnome-disk-utility" "unzip" "zip" "unrar" "7zip" "cpio" "file-roller" "rsync" "nemo" "nemo-fileroller" "tumbler" "ffmpegthumbnailer" "webp-pixbuf-loader" "poppler-glib"
 )
 
 # Group 7: Network & Internet
 pkgs_network=(
-"networkmanager" "iwd" "nm-connection-editor" "inetutils" "wget" "curl" "openssh" "firewalld" "vsftpd" "reflector" "bmon" "ethtool" "httrack" "wavemon" "firefox" "network-manager-applet"
+	"networkmanager" "iwd" "nm-connection-editor" "inetutils" "wget" "curl" "openssh" "firewalld" "vsftpd" "reflector" "bmon" "ethtool" "httrack" "wavemon" "firefox" "network-manager-applet"
 )
 
 # Group 8: Terminal & Shell
 pkgs_terminal=(
-"kitty" "foot" "zsh" "zsh-syntax-highlighting" "starship" "fastfetch" "bat" "eza" "fd" "yazi" "gum" "tree" "fzf" "less" "ripgrep" "expac" "zsh-autosuggestions" "iperf3" "pkgstats" "libqalculate" "moreutils"
+	"kitty" "foot" "zsh" "zsh-syntax-highlighting" "starship" "fastfetch" "bat" "eza" "fd" "yazi" "gum" "tree" "fzf" "less" "ripgrep" "expac" "zsh-autosuggestions" "iperf3" "pkgstats" "libqalculate" "moreutils"
 )
 
 # Group 9: Development
 pkgs_dev=(
-"neovim" "git" "git-delta" "meson" "cmake" "clang" "uv" "rq" "jq" "bc" "viu" "chafa" "ueberzugpp" "ccache" "mold" "shellcheck" "fd" "ripgrep" "fzf" "shfmt" "stylua" "prettier" "tree-sitter-cli" "nano"
+	"neovim" "git" "git-delta" "meson" "cmake" "clang" "uv" "rq" "jq" "bc" "viu" "chafa" "ueberzugpp" "ccache" "mold" "shellcheck" "fd" "ripgrep" "fzf" "shfmt" "stylua" "prettier" "tree-sitter-cli" "nano"
 )
 
 # Group 10: Multimedia
 pkgs_multimedia=(
-"ffmpeg" "mpv" "mpv-mpris" "swappy" "swayimg" "resvg" "imagemagick" "libheif" "ffmpegthumbnailer" "grim" "slurp" "wl-clipboard" "cliphist" "tesseract-data-eng"
+	"ffmpeg" "mpv" "mpv-mpris" "swappy" "swayimg" "resvg" "imagemagick" "libheif" "ffmpegthumbnailer" "grim" "slurp" "wl-clipboard" "cliphist" "tesseract-data-eng"
 )
 
 # Group 11: Sys Admin
 pkgs_sysadmin=(
-"btop" "htop" "dgop" "nvtop" "inxi" "sysstat" "sysbench" "logrotate" "acpid" "tlp" "tlp-pd" "tlp-rdw" "thermald" "powertop" "gdu" "iotop" "iftop" "lshw" "wev" "pacman-contrib" "gnome-keyring" "libsecret" "seahorse" "yad" "dysk" "fwupd"
+	"btop" "htop" "dgop" "nvtop" "inxi" "sysstat" "sysbench" "logrotate" "acpid" "tlp" "tlp-pd" "tlp-rdw" "thermald" "powertop" "gdu" "iotop" "iftop" "lshw" "wev" "pacman-contrib" "gnome-keyring" "libsecret" "seahorse" "yad" "dysk" "fwupd"
 )
 
 # Group 12: Gnome Utilities
 pkgs_gnome=(
-"snapshot" "cameractrls" "loupe" "gnome-text-editor" "gnome-calculator" "gnome-clocks"
+	"snapshot" "cameractrls" "loupe" "gnome-text-editor" "gnome-calculator" "gnome-clocks"
 )
 
 # Group 13: Productivity
 pkgs_productivity=(
-"zathura" "zathura-pdf-mupdf" "cava"
+	"zathura" "zathura-pdf-mupdf" "cava"
 )
 
 # --------------------------------------------------------------------------
@@ -77,8 +77,8 @@ pkgs_productivity=(
 
 # 1. Root Check
 if [[ $EUID -ne 0 ]]; then
-  printf "Elevating privileges...\n"
-  exec sudo "$0" "$@"
+	printf "Elevating privileges...\n"
+	exec sudo "$0" "$@"
 fi
 
 # 2. Safety & Aesthetics
@@ -94,50 +94,50 @@ RESET=$(tput sgr0)
 
 # 3. Core Logic
 install_group() {
-  local group_name="$1"
-  shift
-  local pkgs=("$@")
+	local group_name="$1"
+	shift
+	local pkgs=("$@")
 
-  [[ ${#pkgs[@]} -eq 0 ]] && return
+	[[ ${#pkgs[@]} -eq 0 ]] && return
 
-  printf "\n${BOLD}${CYAN}:: Processing Group: %s${RESET}\n" "$group_name"
+	printf "\n${BOLD}${CYAN}:: Processing Group: %s${RESET}\n" "$group_name"
 
-  # STRATEGY A: Batch Install
-  if pacman -S --needed --noconfirm "${pkgs[@]}"; then
-    printf "${GREEN} [OK] Batch installation successful.${RESET}\n"
-    return 0
-  fi
+	# STRATEGY A: Batch Install
+	if pacman -S --needed --noconfirm "${pkgs[@]}"; then
+		printf "${GREEN} [OK] Batch installation successful.${RESET}\n"
+		return 0
+	fi
 
-  # STRATEGY B: Fallback Individual Install (Smart)
-  printf "\n${YELLOW} [!] Batch transaction failed. Retrying individually...${RESET}\n"
+	# STRATEGY B: Fallback Individual Install (Smart)
+	printf "\n${YELLOW} [!] Batch transaction failed. Retrying individually...${RESET}\n"
 
-  local fail_count=0
+	local fail_count=0
 
-  for pkg in "${pkgs[@]}"; do
-    # Try 1: Auto-install (Silent)
-    # If this works, it means there was no conflict for THIS specific package.
-    if pacman -S --needed --noconfirm "$pkg" >/dev/null 2>&1; then
-      printf "  ${GREEN}[+] Installed:${RESET} %s\n" "$pkg"
-    
-    # Try 2: Interactive (Verbose)
-    # If Auto failed, it's likely a conflict (e.g., tldr vs tealdeer). 
-    # We run without --noconfirm so you can intervene.
-    else
-      printf "  ${YELLOW}[?] Intervention Needed:${RESET} %s\n" "$pkg"
-      if pacman -S --needed "$pkg"; then
-        printf "  ${GREEN}[+] Installed (Manual):${RESET} %s\n" "$pkg"
-      else
-        printf "  ${RED}[X] Not Found / Failed:${RESET} %s\n" "$pkg"
-        ((fail_count++))
-      fi
-    fi
-  done
+	for pkg in "${pkgs[@]}"; do
+		# Try 1: Auto-install (Silent)
+		# If this works, it means there was no conflict for THIS specific package.
+		if pacman -S --needed --noconfirm "$pkg" >/dev/null 2>&1; then
+			printf "  ${GREEN}[+] Installed:${RESET} %s\n" "$pkg"
 
-  if [[ $fail_count -gt 0 ]]; then
-    printf "${YELLOW} [!] Group completed with %d failures.${RESET}\n" "$fail_count"
-  else
-    printf "${GREEN} [OK] Recovery successful. All packages installed.${RESET}\n"
-  fi
+		# Try 2: Interactive (Verbose)
+		# If Auto failed, it's likely a conflict (e.g., tldr vs tealdeer).
+		# We run without --noconfirm so you can intervene.
+		else
+			printf "  ${YELLOW}[?] Intervention Needed:${RESET} %s\n" "$pkg"
+			if pacman -S --needed "$pkg"; then
+				printf "  ${GREEN}[+] Installed (Manual):${RESET} %s\n" "$pkg"
+			else
+				printf "  ${RED}[X] Not Found / Failed:${RESET} %s\n" "$pkg"
+				((fail_count++))
+			fi
+		fi
+	done
+
+	if [[ $fail_count -gt 0 ]]; then
+		printf "${YELLOW} [!] Group completed with %d failures.${RESET}\n" "$fail_count"
+	else
+		printf "${GREEN} [OK] Recovery successful. All packages installed.${RESET}\n"
+	fi
 }
 
 # --- 3. EXECUTION ---
